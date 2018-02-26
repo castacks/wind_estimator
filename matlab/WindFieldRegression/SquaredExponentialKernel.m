282f13e949d4d14a312a0f00d600c38bc26d653d
Julian Lorenz <julianlorenz@live.de>
2018-02-26 14:14:49 -0500

    Removed matlab files from repo

--- matlab/WindFieldRegression/SquaredExponentialKernel.m
+++
@@ -1,8 +0,0 @@
-
-function [result] = SquaredExponentialKernel(x1,x2,l,sigma_f)
-%SquaredExponentialKernel
-%   SquaredExponentialKernel function
-    r = norm(x1-x2);
-    result = sigma_f^2 * exp( -(abs(r))^2 / (2*l^2) );
-end
-
